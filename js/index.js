385d47d9fe21d83605cd3eea4e499d4cc066ebc1
Daniel <danielh989@gmail.com>
2017-04-11 19:58:48 -0400

    First AJAX request to holiday API

--- js/index.js
+++ js/index.js
@@ -5,5 +5,21 @@ $(document).ready(function() {
         result.setDate(result.getDate() + days);
         return result;
     }
-    
+
+    var request = $.ajax({
+            url: "https://holidayapi.com/v1/holidays",
+            method: "GET",
+            data: {
+                key: "62de9748-29e7-4702-a6f4-280d04bcde7f",
+                country: "US",
+                year: "2008"
+            },
+            dataType: "json"
+        });
+        request.done(function(msg) {
+            console.log(msg)
+        });
+        request.fail(function(jqXHR, textStatus) {
+            alert("Request failed: " + textStatus);
+        });
 });
