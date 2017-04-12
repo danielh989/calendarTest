b4590f783f0877f2ae22cf5374be73aa40532979
Daniel <danielh989@gmail.com>
2017-04-11 20:16:39 -0400

    Hidden template for month rendering

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
 $(document).ready(function() {
 
-	function addDays(date, days) {
+    var request = $.ajax({
+        url: "https://holidayapi.com/v1/holidays",
+        method: "GET",
+        data: {
+            key: "62de9748-29e7-4702-a6f4-280d04bcde7f",
+            country: "US",
+            year: "2008"
+        },
+        dataType: "json"
+    });
+    request.done(function(msg) {
+        console.log(msg)
+    });
+    request.fail(function(jqXHR, textStatus) {
+        alert("Request failed: " + textStatus);
+    });
+
+    function addDays(date, days) {
         var result = new Date(date);
         result.setDate(result.getDate() + days);
         return result;
     }
-
-    var request = $.ajax({
-            url: "https://holidayapi.com/v1/holidays",
-            method: "GET",
-            data: {
-                key: "62de9748-29e7-4702-a6f4-280d04bcde7f",
-                country: "US",
-                year: "2008"
-            },
-            dataType: "json"
-        });
-        request.done(function(msg) {
-            console.log(msg)
-        });
-        request.fail(function(jqXHR, textStatus) {
-            alert("Request failed: " + textStatus);
-        });
+    
 });
